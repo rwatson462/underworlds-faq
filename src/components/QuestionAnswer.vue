af3c8f0816b945d96ba1f4582d71022f1376d33c
Rob Watson <source.pot.dev@gmail.com>
2024-01-01 19:03:01 +0000

    Update home page

--- src/components/QuestionAnswer.vue
+++ src/components/QuestionAnswer.vue
@@ -16,18 +16,18 @@
 
 <template>
   <v-card variant="tonal" class="m-4">
-    <v-card-text class="flex gap-2 items-center m-0">
+    <v-card-text class="flex gap-2 items-center">
       <v-icon icon="mdi-chat-question-outline" />
       {{ props.question }}
     </v-card-text>
     <v-card-text class="whitespace-break-spaces">
       A: {{ props.answer }}
     </v-card-text>
-    <v-card-text class="flex gap-4 p-4" v-if="cards?.length || tags?.length">
+    <v-card-text class="flex gap-2 flex-wrap" v-if="cards?.length || tags?.length">
       <VChip size="small" v-for="(card,key) in cards" :key="key" :text="card" variant="flat" />
       <VChip size="small" v-for="(tag,key) in tags" :key="key" :text="tag" variant="tonal" />
     </v-card-text>
-    <v-card-text class="text-sm">
+    <v-card-text class="text-sm bg-grey-50">
       Source: {{ source }}
     </v-card-text>
   </v-card>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -9,7 +9,7 @@ const randomQuestion = {
   question: 'Can Ghartok be driven back?',
   answer: 'No',
   source: 'It is written on his card',
-  tags: ["Magore's Fiends"],
+  tags: ['Magore\'s Fiends']
 }
 
 </script>
@@ -22,35 +22,44 @@ const randomQuestion = {
         Underworlds FAQ is an easy-to-use FAQ presentation for Warhammer Underworlds by Games Workshop, released
         January 1st 2024 and contains Designer's Commentary and Rules updates from December 2023.
       </p>
-      <p><InternalLink :to="route('questions')" text="Get started here" /></p>
+      <p>
+        <InternalLink :to="route('questions')" text="Get started here" />
+      </p>
+      <p>
+        Something to note: Currently this app is up to date with FAQ entries for the Current Season and about half of
+        the Previous Season.  I'll be adding the rest over the next few days.
+      </p>
     </section>
 
-  <section>
-  <p>Here's an inside joke question, but you can see how the entries are formatted:</p>
-  <section class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-    <QuestionAnswer
-      :question="randomQuestion.question"
-      :answer="randomQuestion.answer"
-      :source="randomQuestion.source"
-      :tags="randomQuestion.tags"
-    />
-  </section>
-  </section>
+    <section>
+      <p>Here's an inside joke question, but you can see how the entries are formatted:</p>
+      <section class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <QuestionAnswer
+          :question="randomQuestion.question"
+          :answer="randomQuestion.answer"
+          :source="randomQuestion.source"
+          :tags="randomQuestion.tags"
+        />
+      </section>
+    </section>
 
     <section>
       <p>Sources:</p>
       <ul class="list-disc list-inside">
         <li>
           <ExternalLink to="https://www.warhammer-community.com/wp-content/uploads/2022/10/3o0MaTmdQ3vUgqYa.pdf"
-                        text="Rules Update, December 2023" /> (pdf)
+                        text="Rules Update, December 2023" />
+          (pdf)
         </li>
         <li>
           <ExternalLink to="https://www.warhammer-community.com/wp-content/uploads/2022/10/nC1X3OxTXkVbNMPj.pdf"
-                        text="Designer's Commentary, December 2023" /> (pdf)
+                        text="Designer's Commentary, December 2023" />
+          (pdf)
         </li>
         <li>
           <ExternalLink to="https://www.warhammer-community.com/wp-content/uploads/2022/10/qLeKiJExZOaKQvYZ.pdf"
-                        text="Previous Seasons Designer’s Commentary, December 2023" /> (pdf)
+                        text="Previous Seasons Designer’s Commentary, December 2023" />
+          (pdf)
         </li>
       </ul>
     </section>

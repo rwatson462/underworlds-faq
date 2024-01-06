b66f30ec4956ba285b5a3de571258e42e451e7a7
Rob Watson <source.pot.dev@gmail.com>
2024-01-06 21:36:13 +0000

    Change font to Roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
+    font: {
+      default: 'Roboto'
+    }
   },
   plugins: [],
 }

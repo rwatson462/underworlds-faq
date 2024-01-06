474dc84c40f52827794a553a5db86498fc5e12ad
Rob Watson <source.pot.dev@gmail.com>
2024-01-06 21:35:45 +0000

    Simplify homepage, add latest updates section

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,17 +1,8 @@
 <script setup lang="ts">
-import QuestionAnswer from '@/components/QuestionAnswer.vue'
 import ExternalLink from '@/components/ExternalLink.vue'
 import Heading from '@/components/Heading.vue'
 import InternalLink from '@/components/InternalLink.vue'
 import { route } from '@/helpers/route'
-
-const randomQuestion = {
-  question: 'Can Ghartok be driven back?',
-  answer: 'No',
-  source: 'It is written on his card',
-  tags: ['Magore\'s Fiends']
-}
-
 </script>
 
 <template>
@@ -20,37 +11,51 @@ const randomQuestion = {
       <Heading text="Welcome to Underworlds FAQ" />
       <p>
         Underworlds FAQ is an easy-to-use FAQ presentation for Warhammer Underworlds by Games Workshop, released
-        January 1st 2024 and contains Designer's Commentary and Rules updates from December 2023.
+        January 1st 2024 and contains Designer's Commentary from December 2023.
       </p>
       <p>
         <InternalLink :to="route('questions')" text="Get started here" />
       </p>
-      <p>
-        Something to note: Currently this app is up to date with FAQ entries for the Current Season and about half of
-        the Previous Season.  I'll be adding the rest over the next few days.
-      </p>
     </section>
 
-    <section>
-      <p>Here's an inside joke question, but you can see how the entries are formatted:</p>
-      <section class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <QuestionAnswer
-          :question="randomQuestion.question"
-          :answer="randomQuestion.answer"
-          :source="randomQuestion.source"
-          :tags="randomQuestion.tags"
-        />
-      </section>
+    <section class="space-y-4">
+      <p class="px-2 py-2 text-lg font-semibold">
+        Latest updates
+      </p>
+      <div>
+        <p>
+          <span class="rounded-r px-2 py-1 bg-sky-400 text-white">
+            Saturday 6th January 2024
+          </span>
+        </p>
+        <ul class="list-disc list-outside pl-6 py-1">
+          <li>Added questions up to page 16 of Previous Seasons Designer's Commentary</li>
+          <li>Added this section where I can share latest updates 😀</li>
+          <li>Updated the view of questions to hopefully make them clearer and less dull/grey</li>
+        </ul>
+      </div>
+      <div>
+        <p>
+          <span class="rounded-r px-2 py-1 bg-sky-400 text-white">
+            Monday 1st January 2024
+          </span>
+        </p>
+        <ul class="list-disc list-outside pl-6 py-1">
+          <li>Initial launch!</li>
+          <li>Included all questions from Current Season Designer's Commentary</li>
+          <li>Included pages 1-5 of Previous Seasons Designer's Commentary</li>
+        </ul>
+      </div>
     </section>
 
     <section>
       <p>Sources:</p>
       <ul class="list-disc list-inside">
-        <li>
-          <ExternalLink to="https://www.warhammer-community.com/wp-content/uploads/2022/10/3o0MaTmdQ3vUgqYa.pdf"
-                        text="Rules Update, December 2023" />
-          (pdf)
-        </li>
+<!--        <li>-->
+<!--          <ExternalLink to="https://www.warhammer-community.com/wp-content/uploads/2022/10/3o0MaTmdQ3vUgqYa.pdf"-->
+<!--                        text="Rules Update, December 2023" />-->
+<!--          (pdf)-->
+<!--        </li>-->
         <li>
           <ExternalLink to="https://www.warhammer-community.com/wp-content/uploads/2022/10/nC1X3OxTXkVbNMPj.pdf"
                         text="Designer's Commentary, December 2023" />

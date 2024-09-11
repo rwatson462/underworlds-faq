c7344a2224636101ec3fd3fcbd79fa8562441b89
Rob Watson <source.pot.dev@gmail.com>
2024-09-11 20:54:21 +0100

    Left-align footer text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <v-footer :app="true" elevation="4">
-    <p class="mx-auto text-gray-600 text-sm">&copy; 2024 Rob Watson, Source Pot</p>
+    <p class="text-gray-600 text-sm">&copy; 2024 Rob Watson, Source Pot</p>
   </v-footer>
 </template>
